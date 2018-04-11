96bc428b17de5dad897d6e6bfb08e1cec1f04627
Mitchell Busby <mitchbusby@gmail.com>
2018-04-11 15:43:16 +1000

    Adds barebones context api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+const CityContext = React.createContext(null);
 
-      </div>
+const MyFirstComponent = ({currentCity}) => {
+  return (
+    <p>You are in {currentCity}</p>
+  );
+}
+
+const MyFirstComponentWrapped = () => (
+  <CityContext.Consumer>
+    {
+      value => {
+        console.log('Bobby');
+        return (
+          <MyFirstComponent currentCity={value} />
+        )
+      }
+    }
+  </CityContext.Consumer>
+);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {currentCity: 'Sydney'};
+  }
+  
+  onClick = () => {
+    this.setState({currentCity: 'Rome'});
+  }
+  
+  render() {
+    const {currentCity} = this.state;
+    return (
+        <div className="App">
+          {
+            console.log('Robbie')
+          }
+          <CityContext.Provider value={currentCity}> 
+          <MyFirstComponentWrapped />
+          <button onClick={this.onClick}>
+          Go to Rome
+          </button>
+          {
+            console.log('Jimmy')
+          }
+          </CityContext.Provider>
+        </div>
     );
   }
 }
